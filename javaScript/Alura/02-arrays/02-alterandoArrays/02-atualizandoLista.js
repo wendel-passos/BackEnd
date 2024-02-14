659e0375b49cda85754fdee6a9c5f89db2ba1f95
wendel-passos <wendeleduardo137@gmail.com>
2024-02-13 22:23:29 -0300

    update

---
+++ javaScript/Alura/02-arrays/02-alterandoArrays/02-atualizandoLista.js
@@ -0,0 +1,14 @@
+const listaEstudantes =  ['João', 'Ana', 'Caio', 'Lara', 'Marjorie' ];
+
+listaEstudantes.splice (1,2, 'Rogrigo');
+console.log (listaEstudantes);
+//listaEstudantes.push ('Rodrigo');
+
+
+animaisDoAquario = ['baleia', 'polvo', 'golfinho', 'tubarão'];
+
+animaisDoAquario.splice(1, 0, 'sardinha');
+console.log(animaisDoAquario);
+animaisDoAquario.splice(3, 2, 'atum');
+
+console.log(animaisDoAquario);
---
+++ javaScript/Alura/02-arrays/02-alterandoArrays/03-juntarSalas.js
@@ -0,0 +1,5 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPyton = ['Ju', 'Leo', 'Raquel'];
+
+const salaUnificadas = salaJS.concat(salaPyton);
+console.log (salaUnificadas);
