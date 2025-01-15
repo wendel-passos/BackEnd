619a7f89537691406a24decd21599c1cf3dd6bd5
wendel-passos <wendeleduardo137@gmail.com>
2025-01-14 21:44:18 -0300

    Atualiza exemplo de código no arquivo Program.cs e recompila os arquivos binários.

--- dotnet/exemplosFundamentos/Program.cs
+++ dotnet/exemplosFundamentos/Program.cs
@@ -50,6 +50,25 @@ else
 }
 
 
+//--------Exemplo case------------------
+
+Console.WriteLine ("Digite uma letra");
+string letra = Console.ReadLine();
+
+if (letra == "a" ||
+    letra == "e" ||
+    letra == "i" ||
+    letra == "o" ||
+    letra == "u")
+{
+    Console.WriteLine ("É uma vogal");
+}
+else 
+{
+    Console.WriteLine ("Não é uma vogal");
+}
+
+
 
 
 
