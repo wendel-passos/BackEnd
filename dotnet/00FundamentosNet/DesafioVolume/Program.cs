989c057ca3767351c0bdd917ada7b5ffe0a07878
wendel-passos <wendeleduardo137@gmail.com>
2025-01-22 19:29:53 -0300

    modulo1

---
+++ dotnet/00FundamentosNet/DesafioVolume/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class DIO {
+
+  static void Main(string[] args) {
+
+    double pi, raio, volume;
+    pi = 3.14159;
+
+    // Leitura do raio
+    raio = double.Parse(Console.ReadLine());
+
+    // Cálculo do volume da esfera
+    volume = (4.0 / 3) * pi * Math.Pow(raio, 3);
+
+    // Exibindo o resultado com 3 casas decimais
+    Console.WriteLine($"VOLUME = {volume:F3}");
+  }
+
+}
